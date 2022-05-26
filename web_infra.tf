a69a3def1ba5f7bf49c88ddc2607381244274ebc
Lohan <hsj5009@lgcns.com>
2022-05-26 13:42:12 +0900

    asdf

--- web_infra.tf
+++ web_infra.tf
@@ -46,15 +46,15 @@ resource "aws_lb_listener" "lb_listener_web" {
   protocol          = "HTTP"
 }
 
-resource "aws_lb_target_group_attachment" "lb_attach1" {
-  target_group_arn = aws_lb_target_group.lohan_web_target_group.arn
-  target_id        = aws_instance.Lohan_WEB_1.id
-}
+# resource "aws_lb_target_group_attachment" "lb_attach1" {
+#   target_group_arn = aws_lb_target_group.lohan_web_target_group.arn
+#   target_id        = aws_instance.Lohan_WEB_1.id
+# }
 
-resource "aws_lb_target_group_attachment" "lb_attach2" {
-  target_group_arn = aws_lb_target_group.lohan_web_target_group.arn
-  target_id        = aws_instance.Lohan_WEB_2.id
-}
+# resource "aws_lb_target_group_attachment" "lb_attach2" {
+#   target_group_arn = aws_lb_target_group.lohan_web_target_group.arn
+#   target_id        = aws_instance.Lohan_WEB_2.id
+# }
 
 ##################################################################
 ###AWS WEB-EC2
